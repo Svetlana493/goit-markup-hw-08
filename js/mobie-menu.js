80e977c1bb354d3ffa07752e4bb937fb9eaf0ea3
Svetlana Fedorenko <svetlana493@ukr.net>
2022-01-20 00:08:29 +0200

    спробувала все спочатку створити

--- js/mobie-menu.js
+++ js/mobie-menu.js
@@ -1,17 +1,13 @@
 (() => {
-    const refs = {
-        menuBtn: document.querySelector("[data-menu-button]"),
-        mobileMenu: document.querySelector("[data-menu]"),
-        body: document.querySelector("body"),
-    };
-
-    refs.menuBtn.addEventListener("click", () => {
+    const menuBtnRef = document.querySelector("[data-burger-btn]");
+    const mobileMenuRef = document.querySelector("[data-menu-container]");
+    const body = document.getElementsByTagName("body")[0];
+    menuBtnRef.addEventListener("click", () => {
+        body.classList.toggle("modal-open");
         const expanded =
-            refs.menuBtn.getAttribute("aria-expanded") === "true" || false;
-
-        refs.menuBtn.setAttribute("aria-expanded", !expanded);
-        refs.menuBtn.classList.toggle("is-open");
-        refs.mobileMenu.classList.toggle("is-open");
-        refs.body.classList.toggle("is-open");
+            menuBtnRef.getAttribute("aria-expanded") === "true" || "false";
+        menuBtnRef.classList.toggle("is-opened");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        mobileMenuRef.classList.toggle("is-opened");
     });
 })();
